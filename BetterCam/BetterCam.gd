fdd6dde25c0d3bdad95164335fa0bf8f581109cb
Jeremy <34326862+binaryorange@users.noreply.github.com>
2021-05-16 21:45:36 -0500

    More naming convention fixes

--- BetterCam/BetterCam.gd
+++ BetterCam/BetterCam.gd
@@ -11,49 +11,49 @@ extends Spatial
 
 # Enum descriptions
 """ 
-	CAM_FOLLOW_MODE lets the user choose between an instant camera (like old third-person shooters,
+	CamFollowMode lets the user choose between an instant camera (like old third-person shooters,
 	where the camera would snap instantly either to clip against geometry or to return to its default
 	position behind the player), or a nice smooth camera by using lerping to smoothly position the camera. 
 	
-	CAM_SMOOTH_STYLE lets the user choose between only smoothing out the camera movement when it's 
+	CamSmoothStyle lets the user choose between only smoothing out the camera movement when it's 
 	returning to its non-clipping state, or to allow the camera to pull in and out while smoothing
 	out the camera movement, similar to RDR2.
 	
-	CAM_TARGET lets the user OVERRIDE the default target in favor of a custom one, though this is
+	CamTarget lets the user OVERRIDE the default target in favor of a custom one, though this is
 	NOT recommended, but could be useful if needed.
 	
-	CAM_DISTANCE_MODE lets the user choose between a camera that is positioned at a fixed distance from the player, 
+	CamDistance lets the user choose between a camera that is positioned at a fixed distance from the player, 
 	or a camera that can be positioned dynamically (allowing the player to zoom in/out, for example).
 	
-	INPUT_MODE allows the user to choose how to handle input to the camera. Either only mouse, gamepad, 
+	InputMode allows the user to choose how to handle input to the camera. Either only mouse, gamepad, 
 	or both! Also runs a secondary script ("CheckInputs.gd") that will generate input mappings to control the camera,
 	so that the end-user does not have to manually put in the mappings themselves.
 	
-	INPUTS allows the user to choose whether or not to let BetterCam create the input bindings for them, 
+	Inputs allows the user to choose whether or not to let BetterCam create the input bindings for them, 
 	or if they want to manually add their own. 
 """
-enum CAM_FOLLOW_MODE {INSTANT, SMOOTH} 
-enum CAM_SMOOTH_STYLE {SMOOTH_IN_OUT, SMOOTH_OUT}
-enum CAM_TARGET {DEFAULT, CUSTOM}
-enum CAM_DISTANCE_MODE {FIXED_DISTANCE, DYNAMIC_DISTANCES}
-enum INPUT_MODE {MOUSE, GAMEPAD, MOUSE_AND_GAMEPAD}
-enum INPUTS {CREATE_FOR_ME, CUSTOM}
+enum CamFollowMode {INSTANT, SMOOTH} 
+enum CamSmoothStyle {SMOOTH_IN_OUT, SMOOTH_OUT}
+enum CamTarget {DEFAULT, CUSTOM}
+enum CamDistance {FIXED_DISTANCE, DYNAMIC_DISTANCES}
+enum InputMode {MOUSE, GAMEPAD, MOUSE_AND_GAMEPAD}
+enum Inputs {CREATE_FOR_ME, CUSTOM}
 
 
 # These are publicly available variables that allow the user to choose certain parameters for their camera.
 
 # Camera Settings
-export (CAM_FOLLOW_MODE) var camera_follow_mode = CAM_FOLLOW_MODE.SMOOTH
-export (CAM_SMOOTH_STYLE) var camera_smooth_style = CAM_SMOOTH_STYLE.SMOOTH_OUT
-export (CAM_DISTANCE_MODE) var camera_distance_mode= CAM_DISTANCE_MODE.DYNAMIC_DISTANCES
-export (CAM_TARGET) var camera_target = CAM_TARGET.DEFAULT
+export (CamFollowMode) var camera_follow_mode = CamFollowMode.SMOOTH
+export (CamSmoothStyle) var camera_smooth_style = CamSmoothStyle.SMOOTH_OUT
+export (CamDistance) var camera_distance_mode= CamDistance.DYNAMIC_DISTANCES
+export (CamTarget) var camera_target = CamTarget.DEFAULT
 export var camera_smooth_speed : float = 0.03 
 export var camera_minimum_distance_to_player : float = 1.0 
 export var camera_rotation_speed : float = 1.0  
 
 # Input settings
-export (INPUT_MODE) var camera_input_mode = INPUT_MODE.MOUSE_AND_GAMEPAD
-export (INPUTS) var camera_inputs = INPUTS.CREATE_FOR_ME
+export (InputMode) var camera_input_mode = InputMode.MOUSE_AND_GAMEPAD
+export (Inputs) var camera_inputs = Inputs.CREATE_FOR_ME
 export var mouse_sensitivity : float = 1.0
 export var mouse_deadzone : float = 0.1
 export var joystick_sensitivity : float = 1.0
@@ -75,7 +75,7 @@ var _can_clip : bool = false
 func _ready() -> void:
 	
 	# Determine whether the mouse is free or not
-	if camera_input_mode == INPUT_MODE.MOUSE_AND_GAMEPAD or camera_input_mode == INPUT_MODE.MOUSE:
+	if camera_input_mode == InputMode.MOUSE_AND_GAMEPAD or camera_input_mode == InputMode.MOUSE:
 		Input.set_mouse_mode(Input.MOUSE_MODE_CAPTURED)
 	else:
 		Input.set_mouse_mode(Input.MOUSE_MODE_VISIBLE)
--- BetterCam/CheckInputs.gd
+++ BetterCam/CheckInputs.gd
@@ -13,7 +13,7 @@ onready var BC = $"../"
 # Called when the node enters the scene tree for the first time.
 func _ready() -> void:
 	
-	if BC.camera_inputs == BC.INPUTS.CREATE_FOR_ME:
+	if BC.camera_inputs == BC.Inputs.CREATE_FOR_ME:
 	
 		# We check for every input to prevent possible errors/duplicates.
 		# A little cumbersome but worth it in the end!
@@ -21,55 +21,55 @@ func _ready() -> void:
 		# First check for horizontal rotation, add it if it doesn't exist
 		
 		# Left rotation
-		if !InputMap.has_action("cam_look_left"):
-			InputMap.add_action("cam_look_left", BC.joystick_deadzone)
+		if !InputMap.has_action("_bc_cam_look_left"):
+			InputMap.add_action("_bc_cam_look_left", BC.joystick_deadzone)
 			var event = InputEventJoypadMotion.new()
 			event.axis = JOY_AXIS_2
 			event.axis_value = -1.0
-			InputMap.action_add_event("cam_look_left", event)
+			InputMap.action_add_event("_bc_cam_look_left", event)
 		
 		# Right rotation
-		if !InputMap.has_action("cam_look_right"):
-			InputMap.add_action("cam_look_right", BC.joystick_deadzone)
+		if !InputMap.has_action("_bc_cam_look_right"):
+			InputMap.add_action("_bc_cam_look_right", BC.joystick_deadzone)
 			var event = InputEventJoypadMotion.new()
 			event.axis = JOY_AXIS_2
 			event.axis_value = 1.0
-			InputMap.action_add_event("cam_look_right", event)
+			InputMap.action_add_event("_bc_cam_look_right", event)
 		
 		# Now check for the vertical rotation, if it doesn't exist add it
 		
 		# Up rotation
-		if !InputMap.has_action("cam_look_up"):
-			InputMap.add_action("cam_look_up", BC.joystick_deadzone)
+		if !InputMap.has_action("_bc_cam_look_up"):
+			InputMap.add_action("_bc_cam_look_up", BC.joystick_deadzone)
 			var event = InputEventJoypadMotion.new()
 			event.axis = JOY_AXIS_3
 			event.axis_value = -1.0
-			InputMap.action_add_event("cam_look_up", event)
+			InputMap.action_add_event("_bc_cam_look_up", event)
 		
 		# Down rotation
-		if !InputMap.has_action("cam_look_down"):
-			InputMap.add_action("cam_look_down", BC.joystick_deadzone)
+		if !InputMap.has_action("_bc_cam_look_down"):
+			InputMap.add_action("_bc_cam_look_down", BC.joystick_deadzone)
 			var event = InputEventJoypadMotion.new()
 			event.axis = JOY_AXIS_3
 			event.axis_value = 1.0
-			InputMap.action_add_event("cam_look_down", event)
+			InputMap.action_add_event("_bc_cam_look_down", event)
 		
 		
 		# Now add zoom support
 		
 		# Gamepad bindings
-		if !InputMap.has_action("zoom"):
-			InputMap.add_action("zoom", BC.joystick_deadzone)
+		if !InputMap.has_action("_bc_zoom"):
+			InputMap.add_action("_bc_zoom", BC.joystick_deadzone)
 			var event = InputEventJoypadButton.new()
 			event.button_index = 10
-			InputMap.action_add_event("zoom", event)
+			InputMap.action_add_event("_bc_zoom", event)
 		
 		# Mouse bindings
-		if !InputMap.has_action("mouse_zoom_out"):
-			InputMap.add_action("mouse_zoom_out", BC.mouse_deadzone)
+		if !InputMap.has_action("_bc_mouse_zoom_out"):
+			InputMap.add_action("_bc_mouse_zoom_out", BC.mouse_deadzone)
 			var event = InputEventMouseButton.new()
 			event.button_index = BUTTON_WHEEL_DOWN
-			InputMap.action_add_event("mouse_zoom_out", event)
+			InputMap.action_add_event("_bc_mouse_zoom_out", event)
 			
 		# Now that we're done, inform the user
 		print("Input mappings have been created!")
